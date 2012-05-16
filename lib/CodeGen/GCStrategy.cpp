c571db91e47200c2c12c3c6903f237f5f5d74795
Patrick Walton <pcwalton@mimiga.net>
2012-05-15 17:46:02 -0700

    Automatically insert calls to llvm.gcroot for all allocas that contain pointers in address space 1

--- lib/CodeGen/GCStrategy.cpp
+++ lib/CodeGen/GCStrategy.cpp
@@ -15,6 +15,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/ADT/ArrayRef.h"
 #include "llvm/CodeGen/GCStrategy.h"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/IntrinsicInst.h"
@@ -47,6 +48,9 @@ namespace {
     bool PerformDefaultLowering(Function &F, GCStrategy &Coll);
     static bool InsertRootInitializers(Function &F,
                                        Instruction **Roots, unsigned Count);
+    void AutomaticallyRootValue(AllocaInst &AI, Type *Ty,
+                                ArrayRef<Value *> Indices);
+    void InsertAutomaticGCRoots(Function &F, GCStrategy &S);
     void InsertGCRegisterRoots(Function &F, GCStrategy &S);
     bool IsGCRoot(Value *V);
 
@@ -301,6 +305,75 @@ bool LowerIntrinsics::IsGCRoot(Value *V) {
   return isa<PointerType>(Ty) && cast<PointerType>(Ty)->getAddressSpace() != 0;
 }
 
+void LowerIntrinsics::AutomaticallyRootValue(AllocaInst &AI, Type *Ty,
+                                             ArrayRef<Value *> Indices) {
+  Module *M = AI.getParent()->getParent()->getParent();
+  LLVMContext &C = M->getContext();
+  Type *Int32Ty = Type::getInt32Ty(C);
+
+  switch (Ty->getTypeID()) {
+  case Type::PointerTyID: {
+    if (!cast<PointerType>(Ty)->getAddressSpace() == 1)
+      break;
+
+    // Create the GEP, if necessary.
+    Instruction *BaseInst = &AI;
+    if (Indices.size() > 1) {
+      BaseInst = GetElementPtrInst::Create(&AI, Indices);
+      BaseInst->insertAfter(&AI);
+    }
+
+    // Cast the value to an i8** to make a type-compatible intrinsic call.
+    Type *PtrTy = PointerType::get(Type::getInt8PtrTy(C), 0);
+    Instruction *GCRootArg = new BitCastInst(BaseInst, PtrTy);
+    GCRootArg->insertAfter(BaseInst);
+
+    // Create an intrinsic call.
+    Value *Args[2];
+    Args[0] = GCRootArg;
+    Args[1] = ConstantPointerNull::get(Type::getInt8PtrTy(C));
+    Function *GCRootFn = Intrinsic::getDeclaration(M, Intrinsic::gcroot);
+    CallInst *Call = CallInst::Create(GCRootFn, Args);
+    Call->insertAfter(GCRootArg);
+    break;
+  }
+
+  case Type::StructTyID:
+  case Type::ArrayTyID:
+  case Type::VectorTyID: {
+    SmallVector<Value *, 8> NewIndices(Indices.begin(), Indices.end());
+    NewIndices.push_back(ConstantInt::get(Int32Ty, 0));
+    for (unsigned i = 0; i < Ty->getNumContainedTypes(); ++i) {
+      NewIndices[NewIndices.size() - 1] = ConstantInt::get(Int32Ty, i);
+      AutomaticallyRootValue(AI, Ty->getContainedType(i), NewIndices);
+    }
+    break;
+  }
+
+  default:
+    break;
+  }
+}
+
+void LowerIntrinsics::InsertAutomaticGCRoots(Function &F, GCStrategy &S) {
+  if (!S.usesAutomaticRoots())
+    return;
+
+  LLVMContext &C = F.getParent()->getContext();
+  for (Function::iterator BBI = F.begin(), BBE = F.end(); BBI != BBE; ++BBI) {
+    for (BasicBlock::iterator II = BBI->begin(),
+                              IE = BBI->end(); II != IE; ++II) {
+      AllocaInst *AI = dyn_cast<AllocaInst>(&*II);
+      if (!AI)
+        continue;
+
+      Value *Index = ConstantInt::get(Type::getInt32Ty(C), 0);
+      ArrayRef<Value *> Indices(&Index, 1);
+      AutomaticallyRootValue(*AI, AI->getAllocatedType(), Indices);
+    }
+  }
+}
+
 void LowerIntrinsics::InsertGCRegisterRoots(Function &F, GCStrategy &S) {
   DominatorTree *DT = getAnalysisIfAvailable<DominatorTree>();
   if (!DT)
@@ -382,6 +455,7 @@ void LowerIntrinsics::InsertGCRegisterRoots(Function &F, GCStrategy &S) {
 }
 
 bool LowerIntrinsics::PerformDefaultLowering(Function &F, GCStrategy &S) {
+  InsertAutomaticGCRoots(F, S);
   InsertGCRegisterRoots(F, S);
 
   bool LowerWr = !S.customWriteBarrier();
