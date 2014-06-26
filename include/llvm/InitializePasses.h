8b28e9aba3138b379146d4be759a342fab97a9b5
Cameron Zwarich <zwarich@mozilla.com>
2014-06-25 20:33:49 -0700

    Add a NullCheckElimination pass
    This pass is not Rust-specific, in that all of its transformations are
    intended to be correct for arbitrary LLVM IR, but it targets idioms
    found in IR generated by `rustc`, e.g. heavy use of `inbounds` GEPs.

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -280,6 +280,9 @@ void initializeMachineFunctionPrinterPassPass(PassRegistry&);
 void initializeStackMapLivenessPass(PassRegistry&);
 void initializeMachineCombinerPass(PassRegistry &);
 void initializeLoadCombinePass(PassRegistry&);
+
+// Specific to the rust-lang llvm branch:
+void initializeNullCheckEliminationPass(PassRegistry&);
 }
 
 #endif
