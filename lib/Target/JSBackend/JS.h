c2fabc9938f6bd44213b3ea6cf776e796d913435
Brian Anderson <banderson@mozilla.com>
2016-08-30 20:03:33 -0700

    Import of fastcomp commit 4105790f1549808c1f1daa5250b4ada5f41a5c02
    This is a minimal import of the emscripten "fastcomp" LLVM patchset.
    All it contains is the target definitions necessary to create a
    TargetMachine with the correct data layout. With this rustc can
    emit LLVM IR that emcc will run through the PNaCl lagalizer and
    the JS backend to generate asm.js.

---
+++ lib/Target/JSBackend/JS.h
@@ -0,0 +1,24 @@
+//===-- JS.h - Top-level interface for JS representation ------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in the JS
+// target library, as used by the LLVM JIT.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TARGET_JS_H
+#define TARGET_JS_H
+
+namespace llvm {
+
+class JSTargetMachine;
+
+} // End llvm namespace
+
+#endif
