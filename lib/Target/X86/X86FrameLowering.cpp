a62844c8d9f84a69752a3a99f3f8b169a173f002
bsteinbr <bsteinbr@91177308-0d34-0410-b5e6-96231b3b80d8>
2017-11-07 08:50:21 +0000

    [X86] Don't clobber reserved registers with stack adjustments
    Summary:
    Calls using invoke in funclet based functions are assumed to clobber
    all registers, which causes the stack adjustment using pops to consider
    all registers not defined by the call to be undefined, which can
    unfortunately include the base pointer, if one is needed.
    
    To prevent this (and possibly other hazards), skip reserved registers
    when looking for candidate registers.
    
    This fixes issue #45034 in the Rust compiler.
    
    Reviewers: mkuper
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D39636
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@317551 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Target/X86/X86FrameLowering.cpp
+++ lib/Target/X86/X86FrameLowering.cpp
@@ -2535,6 +2535,7 @@ bool X86FrameLowering::adjustStackWithPops(MachineBasicBlock &MBB,
   unsigned Regs[2];
   unsigned FoundRegs = 0;
 
+  auto &MRI = MBB.getParent()->getRegInfo();
   auto RegMask = Prev->getOperand(1);
 
   auto &RegClass =
@@ -2546,6 +2547,10 @@ bool X86FrameLowering::adjustStackWithPops(MachineBasicBlock &MBB,
     // Since we're immediately after a call, any register that is clobbered
     // by the call and not defined by it can be considered dead.
     if (!RegMask.clobbersPhysReg(Candidate))
+      continue;
+
+    // Don't clobber reserved registers
+    if (MRI.isReserved(Candidate))
       continue;
 
     bool IsDef = false;
